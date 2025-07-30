7f0a7f556221ca5006431c5a613243285cf99dce
kuang51221 <kuang51221@gmail.com>
2025-07-30 16:35:22 +0800

    nothing

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1753851233|14521883842";
+const CACHE_VERSION = "1753864461|1483649721";
 const CACHE_PREFIX = "RtcGdNostr-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
